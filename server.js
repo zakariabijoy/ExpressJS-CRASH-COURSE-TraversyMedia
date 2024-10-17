050e3d93299a46e5dfef1a036f6ef3b47cb1835d
zakariabijoy <zakaria.bijoy@live.com>
2024-10-18 03:33:33 +0600

    Query Strings (req.query)

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ let posts = [
 
 //Get all posts
 app.get("/api/posts", (req, res) => {
-  res.json(posts);
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
 });
 
 //Get single post
