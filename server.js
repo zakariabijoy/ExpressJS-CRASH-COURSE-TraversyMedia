978c8c90a4e65f31f1471a3339b84c5dd7f44d20
zakariabijoy <zakaria.bijoy@live.com>
2024-10-18 03:14:40 +0600

    Static Web Server

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
+// setup static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
