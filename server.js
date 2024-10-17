768884e10eace60435a563eb198288ad864603b2
zakariabijoy <zakaria.bijoy@live.com>
2024-10-18 03:18:28 +0600

    Working with JSON

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const path = require("path");
 const app = express();
 
 // setup static folder
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  { id: 1, title: "Post 1", content: "This is post 1" },
+  { id: 2, title: "Post 2", content: "This is post 2" },
+  { id: 3, title: "Post 3", content: "This is post 3" },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 
 const PORT = process.env.PORT || 5000;
 
