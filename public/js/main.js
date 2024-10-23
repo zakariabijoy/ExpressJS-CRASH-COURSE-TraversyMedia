2ea205f9e379b60ade0b2fe4d1923787ecc98809
zakariabijoy <zakaria.bijoy@live.com>
2024-10-24 02:25:34 +0600

    Submit Form to API

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const output = document.getElementById("output");
 const button = document.getElementById("get-posts-btn");
+const addbutton = document.getElementById("add-post-btn");
 
 async function showPosts() {
   try {
@@ -21,4 +22,34 @@ async function showPosts() {
   }
 }
 
+// submit new post
+
+async function submitPost(event) {
+  event.preventDefault();
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+  const data = { title, content };
+  try {
+    const response = await fetch("http://localhost:8080/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    console.log(result);
+    output.innerHTML = `
+      <div class="card">
+        <h2>${result.title}</h2>
+        <p>${result.content}</p>
+      </div>
+    `;
+    showPosts();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 button.addEventListener("click", showPosts);
+addbutton.addEventListener("click", submitPost);
